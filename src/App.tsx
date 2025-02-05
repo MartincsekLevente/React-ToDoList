8fedb570547a33a40e6d552a9ff33d7e11fa7131
Martincsek Levente <martincsekl@gmail.com>
2025-02-05 21:21:37 +0100

    Make the Add button write the input text to the console

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,50 @@
 import '@mantine/core/styles.css';
-import { Button } from "@mantine/core";
+import './App.scss';
+import { Button, Checkbox, Input } from "@mantine/core";
+import { FormEvent, useState } from "react";
 
 function App() {
+    const [newItem, setNewItem] = useState("");
+
+    function handleSubmit(event: FormEvent<HTMLFormElement>) {
+        event.preventDefault();
+        console.log(newItem)
+    }
+
     return (
-        <>
-            <form>
-                <input/>
-            </form>
-            <Button>Button</Button>
-        </>
+        <div className={"app-container"}>
+            <div className={"new-item-container"}>
+                <div className={"title"}>
+                    New Item
+                </div>
+                <form className={"new-item-form"} onSubmit={handleSubmit}>
+                    <Input
+                        value={newItem}
+                        onChange={event => setNewItem(event.target.value)}
+                        placeholder="Type here..."/>
+                    <Button>
+                        Add Item
+                    </Button>
+                </form>
+            </div>
+            <div className={"to-do-list-container"}>
+                <div className={"title"}>
+                    To-Do List
+                </div>
+                <div className={"to-do-item"}>
+                    <Checkbox label="Do Laundry"></Checkbox>
+                    <Button color="red" variant="outline">
+                        Delete
+                    </Button>
+                </div>
+                <div className={"to-do-item"}>
+                    <Checkbox label="Item 2 here"></Checkbox>
+                    <Button color="red" variant="outline">
+                        Delete
+                    </Button>
+                </div>
+            </div>
+        </div>
     );
 }
 
