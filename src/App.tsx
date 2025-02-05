4113b603afabebab012a943a2c6fdb75c901bb22
Martincsek Levente <martincsekl@gmail.com>
2025-02-05 19:15:16 +0100

    Init

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import '@mantine/core/styles.css';
+import { Button } from "@mantine/core";
+
+function App() {
+    return (
+        <>
+            <form>
+                <input/>
+            </form>
+            <Button>Button</Button>
+        </>
+    );
+}
+
+export default App;
